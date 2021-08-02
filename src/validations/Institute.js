a5305ed459e2583c4832ff2c3f5bd3faaea088a5
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-08-02 19:41:34 +0530

    basic validation

--- src/validations/Institute.js
+++ src/validations/Institute.js
@@ -3,23 +3,29 @@ import * as Yup from "yup";
 const name = Yup.string().required("Name is required.");
 const status = Yup.string().required("Status is required.");
 const type = Yup.string().required("College type is required.");
-// const last_name = Yup.string().required("Last name is required.");
 const full_name = Yup.string().required("First name is required.");
 const assigned_to = Yup.string().required("Assignee is required.");
 const designation = Yup.string().required("Designation is required.");
 const website = Yup.string().required("Website of college is required.");
 const email = Yup.string()
-  .email("Please enter an valid email.")
+  .email("Please enter a valid email.")
   .required("Email is required.");
 
 const phone = Yup.string().required("Phone Number is required.");
-// const city = Yup.string().required("City is required.");
 const state = Yup.string().required("State is required.");
 const medha_area = Yup.string().required("Medha area is required.");
 const address_line = Yup.string().required("Address is required.");
 const pin_code = Yup.number("Should be a number.").required(
   "Pincode is required."
 );
+const address = Yup.object().shape({
+  address_line: Yup.string().required("Address is required."),
+  pin_code: Yup.number("Should be a number.").required(
+    "Pincode is required."
+  ),
+  medha_area: Yup.string().required("Medha area is required."),
+  state: Yup.string().required("State is required."),
+});
 
 export const InstituteValidations = Yup.object({
   name,
@@ -29,13 +35,7 @@ export const InstituteValidations = Yup.object({
   status,
   website,
   // assigned_to,
-});
-
-export const AddressValidations = Yup.object({
-  state,
-  pin_code,
-  medha_area,
-  address_line,
+  address,
 });
 
 export const ContactValidations = Yup.object({
