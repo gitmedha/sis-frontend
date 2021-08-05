8084ee78765c9a8171003315bc7cc2c092f06e83
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-08-05 17:07:21 +0530

    search results container ui

--- src/components/layout/Search/SearchHits.jsx
+++ src/components/layout/Search/SearchHits.jsx
@@ -2,20 +2,63 @@ import styled from "styled-components";
 import { connectHits } from 'react-instantsearch-dom';
 
 const SearchHitsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 15px;
-  background: #FFFFFF;
-  border: 1.5px solid #D7D7E0;
-  box-sizing: border-box;
-  box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.5);
-  border-radius: 5px;
-  width: 800px;
-  position: absolute;
-  margin-top: 5px;
+  table {
+    width: 100%;
+    font-size: 14px;
+    line-height: 18px;
 
-  .hit {
-    margin-bottom: 5px;
+    thead {
+      background-color: #FAFAFE;
+      border-top: 1.5px solid #D7D7E0;
+      border-bottom: 1.5px solid #D7D7E0;
+
+      tr {
+        background: #FAFAFE;
+        border-top: 1.5px solid #D7D7E0;
+        border-bottom: 1.5px solid #D7D7E0;
+      }
+
+      th, td {
+        color: #787B96;
+      }
+    }
+
+    tr {
+      height: 40px;
+
+      th, td {
+        padding-left: 15px;
+        padding-right: 15px;
+      }
+
+      &:not(:last-child) {
+        border-bottom: 1.5px solid #D7D7E0;
+      }
+    }
+  }
+  .badge {
+    height: 24px;
+    padding-left: 12px;
+    padding-right: 12px;
+    color: white;
+    border-radius: 5px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    min-width: auto;
+
+    &.badge-institutions {
+      background-color: #FC636B;
+    }
+    &.badge-students {
+      background-color: #FF9700;
+    }
+    &.badge-employers {
+      background-color: #1AAFD0;
+    }
+    &.badge-batches {
+      background-color: #AA223C;
+    }
   }
 `;
 
@@ -24,9 +67,28 @@ const SearchHits = (props) => {
   let { hits } = props;
   return (
     <SearchHitsContainer>
-      {hits.map(hit => (
-        <div key={hit.id} className="hit">{hit.name}</div>
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Area</th>
+            <th>Assigned To</th>
+          </tr>
+        </thead>
+        <tbody>
+          {hits.map(hit => (
+            <tr key={hit.id} className="hit">
+              <td>
+                <div className="badge badge-institutions">Inst.</div>
+              </td>
+              <td>{hit.name}</td>
+              <td>{hit?.address?.medha_area}</td>
+              <td>{hit?.assigned_to?.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </SearchHitsContainer>
   )
 };
--- src/components/layout/Search/SearchStateResults.jsx
+++ src/components/layout/Search/SearchStateResults.jsx
@@ -1,6 +1,72 @@
+import styled from "styled-components";
 import { connectStateResults } from 'react-instantsearch-dom';
 import SearchHits from './SearchHits';
 import { Highlight } from 'react-instantsearch-dom';
+
+const SearchStateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #FFFFFF;
+  border: 1.5px solid #D7D7E0;
+  box-sizing: border-box;
+  box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+  width: 800px;
+  position: absolute;
+  margin-top: 5px;
+  font-size: 14px;
+  line-height: 18px;
+
+  .header {
+    padding-left: 15px;
+    padding-right: 15px;
+    display: flex;
+    align-items: center;
+  }
+
+  .filter-by-text {
+    font-family: 'Latto-Bold';
+    color: #C4C4C4;
+    margin-right: 15px;
+  }
+  .badges {
+    display: flex;
+    align-items: center;
+
+    .badge {
+      height: 30px;
+      padding-left: 20px;
+      padding-right: 20px;
+      margin-right: 15px;
+      color: white;
+      border-radius: 5px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      min-width: auto;
+
+      &.badge-institutions {
+        background-color: #FC636B;
+      }
+      &.badge-students {
+        background-color: #FF9700;
+      }
+      &.badge-employers {
+        background-color: #1AAFD0;
+      }
+      &.badge-batches {
+        background-color: #AA223C;
+      }
+      &.badge-disabled {
+        background-color: #C4C4C4;
+      }
+    }
+  }
+  .no-results {
+    padding: 15px;
+  }
+`;
 
 const SearchStateResults = (props) => {
   console.log('search state results', props); // debugging on UAT
@@ -9,11 +75,33 @@ const SearchStateResults = (props) => {
   const hasQuery = searchState && searchState.query;
 
   return (
-    <div>
-      <div hidden={!hasResults || !hasQuery}>
-        <SearchHits hitComponent={Hit} />
+    <SearchStateContainer hidden={!hasQuery}>
+      <div className="header pt-4 pb-2">
+        <div className="filter-by-text">Filter by</div>
+        <div className="badges">
+          <div className="badge badge-institutions">
+            Institutions
+          </div>
+          <div className="badge badge-students badge-disabled">
+            Students
+          </div>
+          <div className="badge badge-employers badge-disabled">
+            Employers
+          </div>
+          <div className="badge badge-batches badge-disabled">
+            Batches
+          </div>
+        </div>
       </div>
-    </div>
+      {hasResults ? (
+        <SearchHits hitComponent={Hit} />
+      ) : (
+        <div className="no-results">
+          No results found
+        </div>
+      )}
+
+    </SearchStateContainer>
   );
 };
 
