ffae9a15045dfd098afc54e52e055d4284a61b9d
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-08-12 18:17:28 +0530

    self review

--- src/views/Batches/batchComponents/Details.jsx
+++ src/views/Batches/batchComponents/Details.jsx
@@ -46,12 +46,9 @@ const Details = ({ batch, sessions=[] }) => {
   ];
 
   let totalSessionAttendancePercentages = 0;
-  console.log('sessions',sessions);
   sessions.map(session => {
-    console.log('session.percent', session.percent);
     return totalSessionAttendancePercentages += session.percent;
   });
-  console.log('totalSessionAttendancePercentages', totalSessionAttendancePercentages);
   const averageAttendancePercent = Math.ceil(totalSessionAttendancePercentages / sessions.length);
 
   return (
