192bad4abe04d077bd5147a8c87e3145921b6b5a
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-09-10 23:39:20 +0530

    remove extra code

--- src/validations/Batch.js
+++ src/validations/Batch.js
@@ -1,16 +1,16 @@
 import * as Yup from "yup";
 
 const topics = Yup.string().required("Topic covered is required.");
-const date = Yup.date() .nullable() .default(null) .required("Session Date is required.");
+const date = Yup.date().nullable().required("Session Date is required.");
 
 // New Batch Validation Rules
 const name = Yup.string().required("Name is required.");
 const assigned_to = Yup.string().required("Assignee is required.");
 const grant = Yup.string().required("Grant for batch is required.");
 const status = Yup.string().required("Status of batch is required.");
-const end_date = Yup.date() .nullable() .default(null) .required("End Date of batch is required.");
+const end_date = Yup.date().nullable().required("End Date of batch is required.");
 const program = Yup.string().required("Program for the batch is required.");
-const start_date = Yup.date() .nullable() .default(null) .required("Start Date of batch is required.");
+const start_date = Yup.date().nullable().required("Start Date of batch is required.");
 const institution = Yup.string().required("Institution for batch is required.");
 const per_student_fees = Yup.string().required("Fees per student is required.");
 const seats_available = Yup.string().required("Seats Available is required.");
