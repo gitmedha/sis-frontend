bc118f857579f416aa69221ba48048e1aff1b24f
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-09-10 22:11:38 +0530

    resolve employment connection validations

--- src/validations/Student.js
+++ src/validations/Student.js
@@ -10,6 +10,12 @@ const course_name_in_current_sis = Yup.string().required("Course Name is require
 const course_type = Yup.string().required("Course Type is required.");
 const registration_date = Yup.string().required("Registration Date is required.");
 const fee_status = Yup.string().required("Fee Status is required.");
+const start_date = Yup.date() .nullable() .default(null).required("Start Date is required.");
+const end_date = Yup.date() .nullable() .default(null).required("End Date is required.");
+const employer_id = Yup.string().required("Employer is required.");
+const opportunity_id = Yup.string().required("Opportunity is required.");
+const source = Yup.string().required("Source is required.");
+const salary_offered = Yup.string().required("Salary Offered is required.");
 
 export const ProgramEnrollmentValidations = Yup.object({
   institution,
@@ -22,4 +28,14 @@ export const ProgramEnrollmentValidations = Yup.object({
   year_of_course_completion,
   registration_date,
   fee_status
+});
+
+export const EmploymentConnectionValidations = Yup.object({
+  start_date,
+  end_date,
+  status,
+  employer_id,
+  opportunity_id,
+  source,
+  salary_offered,
 });
