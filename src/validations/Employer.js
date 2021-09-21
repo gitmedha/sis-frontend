cd20714c4dcd5d4cd393d48f9a06a6920032cdb2
Shubham Tiwari <shubham.tiwari@coloredcow.com>
2021-09-21 16:05:58 +0530

    fix pincode validation

--- src/validations/Employer.js
+++ src/validations/Employer.js
@@ -1,6 +1,7 @@
 import * as Yup from "yup";
 
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+const pincodeRegExp = /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/
 const name = Yup.string().required("Name is required.");
 const status = Yup.string().required("Status is required.");
 const full_name = Yup.string().required("Name is required.");
@@ -9,7 +10,7 @@ const designation = Yup.string().required("Designation is required.");
 const email = Yup.string()
   .email("Please enter a valid email.")
   .required("Email is required.");
-  const phone = Yup.string()
+const phone = Yup.string()
   .matches(phoneRegExp, 'Phone number is not valid')
   .min(10, "Number is too short")
   .max(10, "Number is too long")
@@ -18,7 +19,7 @@ const state = Yup.string().required("State is required.");
 const medha_area = Yup.string().required("Medha area is required.");
 const address = Yup.string().required("Address is required.");
 const pin_code = Yup.string("Should be a number.")
-  .matches(phoneRegExp, 'Pincode is not valid')
+  .matches(pincodeRegExp, 'Pincode is not valid')
   .max(6, "Number is too long")
   .required("Pincode is required.");
 const type = Yup.string().required("Type is required.");
