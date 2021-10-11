71b90063ab1f7be6e84964533644be074e04f579
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-10-11 13:57:12 +0530

    dashboard fix

--- src/views/Students/StudentComponents/EmploymentConnections.jsx
+++ src/views/Students/StudentComponents/EmploymentConnections.jsx
@@ -31,6 +31,7 @@ const EmploymentConnections = (props) => {
   const {setAlert} = props;
   const [employmentConnectionsTableData, setEmploymentConnectionsTableData] = useState(employmentConnections);
   const [selectedEmploymentConnection, setSelectedEmploymentConnection] = useState({});
+  const userId = localStorage.getItem("userId") || 2;
 
   useEffect(() => {
     getEmploymentConnectionsPickList().then(data => {
@@ -116,7 +117,7 @@ const EmploymentConnections = (props) => {
     dataToSave['salary_offered'] = data.salary_offered ? Number(data.salary_offered) : null;
     dataToSave['opportunity'] = data.opportunity_id;
     dataToSave['student'] = student.id;
-    dataToSave['assigned_to'] = 2; // to be changed
+    dataToSave['assigned_to'] = userId;
 
     createEmploymentConnection(dataToSave).then(data => {
       setAlert("Employment Connection created successfully.", "success");
@@ -141,7 +142,7 @@ const EmploymentConnections = (props) => {
     dataToSave['end_date'] = data.end_date ? moment(data.end_date).format("YYYY-MM-DD") : null;
     dataToSave['salary_offered'] = data.salary_offered ? Number(data.salary_offered) : null;
     dataToSave['opportunity'] = data.opportunity_id;
-    dataToSave['assigned_to'] = 2; // to be changed
+    dataToSave['assigned_to'] = userId;
 
     updateEmploymentConnection(Number(id), dataToSave).then(data => {
       setAlert("Employment Connection updated successfully.", "success");
