e71f3043bbf38825da3770eae2b0c0bcad8e156a
Shubham Tiwari <shubham.tiwari@coloredcow.com>
2021-10-12 13:32:16 +0530

    add assigned to condition

--- src/graphql/opportunities.js
+++ src/graphql/opportunities.js
@@ -60,13 +60,16 @@ const employmentConnectionFields = `
 `;
 
 export const GET_OPPORTUNITIES = `
-query GET_OPPORTUNITIES($limit: Int, $start: Int, $sort: String, $status: String) {
+query GET_OPPORTUNITIES($id: Int, $limit: Int, $start: Int, $sort: String, $status: String) {
 opportunitiesConnection(
   sort: $sort
   start: $start
   limit: $limit
   where: {
-    status: $status
+    assigned_to: {
+      id: $id
+    },
+    status:$status
   }
 ) {
   values {
--- src/graphql/student.js
+++ src/graphql/student.js
@@ -99,13 +99,16 @@ const employmentConnectionFields = `
 `;
 
 export const GET_STUDENTS = `
-  query GET_STUDENTS($limit: Int, $start: Int, $sort: String, $status: String) {
+  query GET_STUDENTS($id: Int, $limit: Int, $start: Int, $sort: String, $status:String) {
     studentsConnection (
       sort: $sort
       start: $start
       limit: $limit
       where: {
-        status: $status
+        assigned_to: {
+          id: $id
+        },
+        status:$status
       }
     ) {
       values {
