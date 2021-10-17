b8afd066b096fd6803b136f49de060b1a17d6dbb
Shubham Tiwari <shubham.tiwari@coloredcow.com>
2021-10-17 13:28:10 +0530

    dynamic data in registration charts

--- src/components/content/Chart.jsx
+++ src/components/content/Chart.jsx
@@ -1,6 +1,7 @@
 import Chart from "react-apexcharts";
 import { useState, useEffect } from "react";
 import { getMyDataMetricsGraph } from "../../views/Dashboard/components/DashboardActions";
+import moment from "moment";
 
 const ProgramEnrollmentChart = (props) => {
   const userId = Number(localStorage.getItem("user_id")) || 2;
@@ -31,44 +32,64 @@ const ProgramEnrollmentChart = (props) => {
         stops: [0, 90, 100]
       }
     },
-    xaxis: {
-      categories: [
-        "Jan 2021",
-        "Feb 2021",
-        "Mar 2021",
-        "Apr 2021",
-        "May 2021",
-        "Jun 2021",
-        "Aug 2021"
-      ]
-    }
   });
   const [series, setSeries] = useState([
     {
       name: "Registrations",
-      data: [30000, 52000, 38000, 45000, 19000, 23000, 2000]
+      data: []
     },
     {
       name: "Certifications",
-      data: [35000, 35000, 40000, 25000, 44000, 55000, 75000],
+      data: []
     }
   ]);
 
   useEffect(() => {
     getMyDataMetricsGraph(userId, 'registrations').then(data => {
-      let registrationDataArray = data.data.data.programEnrollmentsConnection.groupBy.registration_date;
+      const sixMonthsAgo = new Date();
+      sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+      let registrationDataArray = (data.data.data.programEnrollmentsConnection.groupBy.registration_date).filter(({key}) => (new Date(key)) > sixMonthsAgo);
+
       // set key = date and value = count
       let registrationData = {};
       registrationDataArray.map(item => {
         registrationData[item.key] = item.connection.aggregate.count;
       });
       // sort the data by date
-      registrationData = Object.keys(registrationData).sort().reduce((obj, key) => {
-        obj[key] = registrationData[key];
-        return obj;
-      }, {});
-      console.log('sorted registrationData', registrationData);
+      // registrationData = Object.keys(registrationData).sort().reduce((obj, key) => {
+      //   obj[key] = registrationData[key];
+      //   return obj;
+      // }, {});
+      
+      let aggregateDate = [];
+      Object.keys(registrationData).sort().forEach(date => {
+        let registrationCount = registrationData[date];
+        let yearMonth = moment(date).format('yy-MMM');
+        
+        if (aggregateDate[yearMonth] === undefined) {
+          aggregateDate[yearMonth] = registrationCount; 
+        } else {
+          aggregateDate[yearMonth] += registrationCount; 
+        }
+        
+        setOptions({
+          xaxis: {
+            categories: Object.keys(aggregateDate)
+          }
+        })
+        
+        setSeries([{
+          name: "Registrations",
+          data: Object.values(aggregateDate)
+        },
+        {
+          name: "Registrations",
+          data: ["1", "3", "5","2"]
+        }
+      ])
     });
+  });
+
   }, []);
 
   return (
