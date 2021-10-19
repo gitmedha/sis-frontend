28841798cad97a059861a9dd742be18e5980a77b
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-10-19 10:53:20 +0530

    dashboard metrics fix

--- src/graphql/dashboard.js
+++ src/graphql/dashboard.js
@@ -51,7 +51,7 @@ export const GET_MY_DATA_REGISTRATIONS = `
             id: $user
           }
         }
-        status_nin: ["Batch Assigned", "Batch Complete", "Certified by Medha"]
+        status_in: ["Batch Assigned", "Batch Complete", "Certified by Medha"]
       }
     ) {
       aggregate {
@@ -245,7 +245,7 @@ export const GET_MY_DATA_REGISTRATIONS_GRAPH = `
             id: $user
           }
         }
-        status_nin: ["Batch Assigned", "Batch Complete", "Certified by Medha"]
+        status_in: ["Batch Assigned", "Batch Complete", "Certified by Medha"]
       }
     ) {
       groupBy {
