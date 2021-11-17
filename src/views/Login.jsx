8a062feccaaa3891db5dd2c122221897fbc25a3b
Shubham Tiwari <shubham.tiwari@coloredcow.com>
2021-11-17 08:26:26 +0530

    add image

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -11,31 +11,42 @@ const Styled = styled.div`
       align-items: center;
       justify-content: center;
       padding-bottom: 120px;
+      border:7px solid red;
     }
   }
 
   .logo {
+  padding: 5px;
+  width: 108px;
+  height: 121px;
+  margin-left: -30px;
+ }
+  .image {
+    border:2px solid green;
     max-width: 100%;
+    height: 103%;
+    margin: 0px 0px 0px 111px;
   }
 
-  .app-name {
-    color: #207B69;
-    font-family: 'Latto-Bold';
-    font-size: 20px;
+  .app-name, p {
+    color: black;
+    font-family: 'Bebas Neue Book';
+    font-size: 50px;
     line-height: 1.25;
-    margin-bottom: 60px;
+    margin: 75px 39px 21px -19px;
   }
 
   .btn-ms-login {
-    background-color: #EEEFF8;
     border: 1px solid #C4C4C4;
     box-sizing: border-box;
-    border-radius: 10px;
+    border-radius: 40px;
     padding: 20px 45px;
     color: #787B96;
-    font-family: 'Latto-Bold';
+    font-family: 'Lato Regular';
     text-decoration: none;
-
+    position: absolute;
+    left:   80px;
+    bottom:   5px;
     img {
       width: 30px;
       margin-right: 20px;
@@ -48,16 +59,14 @@ const Login = () => {
   return (
     <Styled>
       <div className="container">
-        <div className="row content-wrapper">
-          <div className="col-md-6">
+        <div className="row">
+          <div className="col-md-4">
             <img
-              src={require('../assets/images/logo-sharp.png').default}
-              alt="Medha SIS"
-              className='logo'
+                src={require('../assets/images/logo.png').default}
+                alt="Medha SIS"
+                className='logo'
             />
-          </div>
-          <div className="col-md-6">
-            <p className="app-name">Student Information System</p>
+            <p className="app-name">STUDENT <br/> INFORMATION <br/> SYSTEM</p>
             <a type="button" href={urlPath('/connect/microsoft')} className="btn-ms-login d-flex">
               <img
                 src={require('../assets/images/logo-microsoft.svg').default}
@@ -67,6 +76,13 @@ const Login = () => {
               <span>Login using Microsoft account</span>
             </a>
           </div>
+          <div className="col-md-8">
+            <img
+             src={require('../assets/images/web-image.png').default}
+             alt="Medha SIS"
+             className='image'
+            />
+          </div> 
         </div>
       </div>
     </Styled>
