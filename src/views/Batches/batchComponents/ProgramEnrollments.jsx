3bcd015200070ca9b095322faef1fce37aff824a
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-03-16 16:33:46 +0530

    putting file key in ignored data when updating

--- src/views/Batches/batchComponents/ProgramEnrollments.jsx
+++ src/views/Batches/batchComponents/ProgramEnrollments.jsx
@@ -241,7 +241,7 @@ const ProgramEnrollments = (props) => {
     }
 
     // need to remove some data from the payload that's not accepted by the API
-    let { id, attendancePercent, attendance, student_id, program_name, updated_at, created_at, certification_date_formatted, medha_program_certificate, medha_program_certificate_icon, program_enrollment_batch, registration_date_formatted, student_name, batch_name, institution_name,  status_badge, fee_status_badge, ...dataToSave} = data;
+    let { id, attendancePercent, attendance, student_id, program_name, updated_at, created_at, certification_date_formatted, medha_program_certificate, medha_program_certificate_icon, program_enrollment_batch, registration_date_formatted, student_name, batch_name, institution_name,  status_badge, fee_status_badge, higher_education_proof_of_enrollment, ...dataToSave} = data;
     dataToSave['registration_date'] = data.registration_date ? moment(data.registration_date).format("YYYY-MM-DD") : null;
     dataToSave['certification_date'] = data.certification_date ? moment(data.certification_date).format("YYYY-MM-DD") : null;
     dataToSave['fee_payment_date'] = data.fee_payment_date ? moment(data.fee_payment_date).format("YYYY-MM-DD") : null;
