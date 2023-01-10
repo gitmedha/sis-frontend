3b1345c6b031fd5e75d06701efe5b2ed84735a57
Snehil Raj <snehil.raj@coloredcow.in>
2023-01-10 20:11:34 +0530

    fixed date format

--- src/views/Institutions/InstitutionComponents/MoUs.jsx
+++ src/views/Institutions/InstitutionComponents/MoUs.jsx
@@ -1,26 +1,24 @@
 import Table from "../../../components/content/Table";
 
 const MoUs = ({ mou }) => {
-  if (Array.isArray(mou) && mou.length > 0) {
-    mou = mou.map((mou_obj) => {
-      if (mou_obj.mou_file.hasOwnProperty("url")) {
-        let url = mou_obj.mou_file.url;
-        if (typeof url === "string") {
-          let file_name = url.split("/").pop();
-          mou_obj.mou_file.url = (
-            <a href={url} target="_blank" rel="noopener noreferrer">
-              {file_name}
-            </a>
-          );
-        }
-      } else {
-        mou_obj.mou_file.url = "No Data Found";
+  mou = mou.map((mou_obj) => {
+    if (mou_obj.mou_file.hasOwnProperty("url")) {
+      let url = mou_obj.mou_file.url;
+      if (typeof url === "string") {
+        let file_name = url.split("/").pop();
+        mou_obj.mou_file.url = (
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {file_name}
+          </a>
+        );
       }
-      return mou_obj;
-    });
-  } else {
-    console.log("No Data found");
-  }
+    } else {
+      mou_obj.mou_file.url = "No Data Found";
+    }
+    mou_obj.start_date = new Date(mou_obj.start_date).toLocaleDateString();
+    mou_obj.end_date = new Date(mou_obj.end_date).toLocaleDateString();
+    return mou_obj;
+  });
   const columns = [
     { Header: "Name", accessor: "mou_file.url" },
     { Header: "Start Date", accessor: "start_date" },
