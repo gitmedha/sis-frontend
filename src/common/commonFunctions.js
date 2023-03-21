317f4463ece78f2f791b5b1f5ba98fbecb899f2a
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-03-21 13:12:24 +0530

    check for local storage value

--- src/common/commonFunctions.js
+++ src/common/commonFunctions.js
@@ -1,14 +1,14 @@
 export const isSRM = () => {
   const userRole = localStorage.getItem('user_role');
-  return userRole.toLowerCase() === 'basic';
+  return userRole && userRole.toLowerCase() === 'basic';
 }
 
 export const isAdmin = () => {
   const userRole = localStorage.getItem('user_role');
-  return userRole.toLowerCase() === 'admin';
+  return userRole && userRole.toLowerCase() === 'admin';
 }
 
 export const isChapterHead = () => {
   const userRole = localStorage.getItem('user_role');
-  return userRole.toLowerCase() === 'chapter head';
+  return userRole && userRole.toLowerCase() === 'chapter head';
 }
