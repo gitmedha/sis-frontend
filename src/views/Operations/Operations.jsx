81e32ef58fea59eaec5343d3b933355d0f9a482c
rohit-shamra-medha <rohit.sharma@medha.org.in>
2023-07-26 16:42:58 +0530

    added user tot

--- src/views/Operations/Operations.jsx
+++ src/views/Operations/Operations.jsx
@@ -1,5 +1,5 @@
 import nProgress from "nprogress";
-import styled from 'styled-components';
+import styled from "styled-components";
 import api from "../../apis";
 import {
   TableRowDetailLink,
@@ -11,10 +11,10 @@ import moment from "moment";
 import { connect } from "react-redux";
 import { useState, useEffect, useMemo, useCallback } from "react";
 import { useHistory } from "react-router-dom";
-import { GET_OPERATIONS } from "../../graphql";
+import { GET_OPERATIONS, GET_USERSTOTS } from "../../graphql";
 import TabPicker from "../../components/content/TabPicker";
 import Tabs from "../../components/content/Tabs";
-import Table from '../../components/content/Table';
+import Table from "../../components/content/Table";
 import { setAlert } from "../../store/reducers/Notifications/actions";
 import Collapse from "../../components/content/CollapsiblePanels";
 import { isAdmin, isSRM } from "../../common/commonFunctions";
@@ -29,16 +29,16 @@ const tabPickerOptions = [
 ];
 
 const Styled = styled.div`
-
-  .MuiSwitch-root { // material switch
+  .MuiSwitch-root {
+    // material switch
     margin-left: 5px;
     margin-right: 5px;
 
     .MuiSwitch-switchBase {
-      color: #207B69;
+      color: #207b69;
     }
     .MuiSwitch-track {
-      background-color: #C4C4C4;
+      background-color: #c4c4c4;
       opacity: 1;
     }
   }
@@ -53,64 +53,59 @@ const Operations = (props) => {
   const [opts, setOpts] = useState([]);
   const [optsdata, setOptsdata] = useState({});
   const [optsAggregate, setoptsAggregate] = useState([]);
-  const [studentsData, setStudentsData] = useState([]);
-  const [pickList, setPickList] = useState([]);
-  const [students, setStudents] = useState([]);
   const [modalShow, setModalShow] = useState(false);
-  const [layout, setLayout] = useState('list');
+  const [layout, setLayout] = useState("list");
   const [activeTab, setActiveTab] = useState(tabPickerOptions[0]);
-  const [activeStatus, setActiveStatus] = useState('All');
-  const pageSize = parseInt(localStorage.getItem('tablePageSize')) || 25;
+  const [activeStatus, setActiveStatus] = useState("All");
+  const pageSize = parseInt(localStorage.getItem("tablePageSize")) || 25;
   const [paginationPageSize, setPaginationPageSize] = useState(pageSize);
   const [paginationPageIndex, setPaginationPageIndex] = useState(0);
-  const userId = parseInt(localStorage.getItem('user_id'))
-  const state = localStorage.getItem('user_state');
-  const area = localStorage.getItem('user_area')
+  const userId = parseInt(localStorage.getItem("user_id"));
+  const state = localStorage.getItem("user_state");
+  const area = localStorage.getItem("user_area");
 
-  
   const columns = useMemo(
     () => [
       {
-        Header: 'Assigned To',
-        accessor: 'assigned_to.username',
+        Header: "Assigned To",
+        accessor: "assigned_to.username",
       },
       {
-        Header: 'Activity type',
-        accessor: 'activity_type',
+        Header: "Activity type",
+        accessor: "activity_type",
       },
-      
+
       {
-        Header: 'Area',
-        accessor: 'area',
+        Header: "Area",
+        accessor: "area",
       },
-      
+
       {
-        Header: 'Batch',
-        accessor: 'batch.name',
-      },
-      {
-        Header: 'Start Date',
-        accessor: 'start_date',
+        Header: "Batch",
+        accessor: "batch.name",
       },
       {
-        Header: 'End Date',
-        accessor: 'end_date',
+        Header: "Start Date",
+        accessor: "start_date",
       },
       {
-        Header: 'Topic',
-        accessor: 'topic',
+        Header: "End Date",
+        accessor: "end_date",
       },
-     
       {
-        Header: 'Guest',
-        accessor: 'guest',
+        Header: "Topic",
+        accessor: "topic",
       },
-     
+
       {
-        Header: 'Organization',
-        accessor: 'organization',
+        Header: "Guest",
+        accessor: "guest",
       },
-      
+
+      {
+        Header: "Organization",
+        accessor: "organization",
+      },
     ],
     []
   );
@@ -118,113 +113,177 @@ const Operations = (props) => {
   const columnsUserTot = useMemo(
     () => [
       {
-        Header: 'Assigned To',
-        accessor: 'assigned_to.username',
+        Header: "User Name",
+        accessor: "user_name",
       },
       {
-        Header: 'Activity type',
-        accessor: 'activity_type',
+        Header: "City",
+        accessor: "city",
       },
-      
+
       {
-        Header: 'Area',
-        accessor: 'area',
+        Header: "Propject Name",
+        accessor: "project_name",
       },
-      
+
       {
-        Header: 'Batch',
-        accessor: 'batch.name',
-      },
-      {
-        Header: 'Start Date',
-        accessor: 'start_date',
+        Header: "Batch",
+        accessor: "partner_dept",
       },
       {
-        Header: 'End Date',
-        accessor: 'end_date',
+        Header: "Start Date",
+        accessor: "start_date",
       },
       {
-        Header: 'Topic',
-        accessor: 'topic',
+        Header: "End Date",
+        accessor: "module_name",
       },
-     
-      {
-        Header: 'Guest',
-        accessor: 'guest',
-      },
-     
-      {
-        Header: 'Organization',
-        accessor: 'organization',
-      },
-      
+      // {
+      //   Header: 'Topic',
+      //   accessor: 'topic',
+      // },
+
+      // {
+      //   Header: 'Guest',
+      //   accessor: 'guest',
+      // },
+
+      // {
+      //   Header: 'Organization',
+      //   accessor: 'organization',
+      // },
     ],
     []
   );
 
-  const getoperations = async (status = 'All', selectedTab, limit = paginationPageSize, offset = 0, sortBy = 'created_at', sortOrder = 'desc') => {
-
+  const getoperations = async (
+    status = "All",
+    selectedTab,
+    limit = paginationPageSize,
+    offset = 0,
+    sortBy = "created_at",
+    sortOrder = "desc"
+  ) => {
     nProgress.start();
     setLoading(true);
     let variables = {
       limit,
       start: offset,
       sort: `${sortBy}:${sortOrder}`,
+    };
+    if (activeTab.key == "my_data") {
+      await api
+        .post("/graphql", {
+          query: GET_OPERATIONS,
+          variables,
+        })
+        .then((data) => {
+          console.log(
+            "data",
+            data.data.data.usersOpsActivitiesConnection.values
+          );
+          setOpts(data.data.data.usersOpsActivitiesConnection.values);
+          setoptsAggregate(
+            data.data.data.usersOpsActivitiesConnection.aggregate
+          );
+        })
+        .catch((error) => {
+          return Promise.reject(error);
+        })
+        .finally(() => {
+          setLoading(false);
+          nProgress.done();
+        });
     }
-    await api.post("/graphql", {
-      query: GET_OPERATIONS,
-      variables,
-    })
-      .then(data => {
-        console.log("data",data.data.data.usersOpsActivitiesConnection.values)
-        setOpts(data.data.data.usersOpsActivitiesConnection.values);
-        setoptsAggregate(data.data.data.usersOpsActivitiesConnection.aggregate)
-      })
-      .catch(error => {
-        return Promise.reject(error);
-      })
-      .finally(() => {
-        setLoading(false);
-        nProgress.done();
-      });
+    if (activeTab.key == "useTot") {
+      await api
+        .post("/graphql", {
+          query: GET_USERSTOTS,
+          variables,
+        })
+        .then((data) => {
+          console.log("data12", data.data.data.usersTotsConnection.values);
+          setOpts(data.data.data.usersTotsConnection.values);
+          // setoptsAggregate(data.data.data.usersOpsActivitiesConnection.aggregate)
+        })
+        .catch((error) => {
+          return Promise.reject(error);
+        })
+        .finally(() => {
+          setLoading(false);
+          nProgress.done();
+        });
+    }
+
+    if (activeTab.key == "Upskilling") {
+      await api
+        .post("/graphql", {
+          query: GET_USERSTOTS,
+          variables,
+        })
+        .then((data) => {
+          console.log("data12", data.data.data.usersTotsConnection.values);
+          setOpts(data.data.data.usersTotsConnection.values);
+          // setoptsAggregate(data.data.data.usersOpsActivitiesConnection.aggregate)
+        })
+        .catch((error) => {
+          return Promise.reject(error);
+        })
+        .finally(() => {
+          setLoading(false);
+          nProgress.done();
+        });
+    }
   };
 
-  const fetchData = useCallback((pageIndex, pageSize, sortBy) => {
-    if (sortBy.length) {
-      let sortByField = 'full_name';
-      let sortOrder = sortBy[0].desc === true ? 'desc' : 'asc';
-      switch (sortBy[0].id) {
-        case 'area':
-        case 'assigned_to.username':
-        case 'activity_type':
-        case 'batch.name':
-          sortByField = sortBy[0].id;
-          break;
+  const fetchData = useCallback(
+    (pageIndex, pageSize, sortBy) => {
+      if (sortBy.length) {
+        let sortByField = "full_name";
+        let sortOrder = sortBy[0].desc === true ? "desc" : "asc";
+        switch (sortBy[0].id) {
+          case "area":
+          case "assigned_to.username":
+          case "activity_type":
+          case "batch.name":
+            sortByField = sortBy[0].id;
+            break;
 
-        default:
-          sortByField = 'full_name';
-          break;
+          default:
+            sortByField = "full_name";
+            break;
+        }
+
+        getoperations(
+          activeStatus,
+          activeTab.key,
+          pageSize,
+          pageSize * pageIndex,
+          sortByField,
+          sortOrder
+        );
+      } else {
+        getoperations(
+          activeStatus,
+          activeTab.key,
+          pageSize,
+          pageSize * pageIndex
+        );
       }
-
-      getoperations(activeStatus, activeTab.key, pageSize, pageSize * pageIndex, sortByField, sortOrder);
-    } else {
-      getoperations(activeStatus, activeTab.key, pageSize, pageSize * pageIndex);
-    }
-  }, [activeTab.key, activeStatus]);
+    },
+    [activeTab.key, activeStatus]
+  );
 
   useEffect(() => {
     // getoperationsPickList().then(data => setPickList(data));
     console.log("activeTab_Key", activeTab.key);
     console.log("tabPickerOptions", tabPickerOptions);
     fetchData(0, paginationPageSize, []);
-  }, []);
- 
+  }, [activeTab]);
 
   useEffect(() => {
     setPaginationPageIndex(0);
   }, [activeTab.key, activeStatus]);
-
-
 
   const hideShowModal = async (data) => {
     if (!data || data.isTrusted) {
@@ -237,45 +296,67 @@ const Operations = (props) => {
       setModalShow(false);
       return;
     }
-
-    
   };
 
- 
   const showRowData = (data) => {
-    setOptsdata(data)
+    setOptsdata(data);
     setShowModal(true);
-  }
+  };
 
   return (
     <Collapse title="OPERATIONS" type="plain" opened={true}>
       <Styled>
         <div className="row m-1">
-          
           <div className="d-flex flex-column flex-md-row justify-content-between align-items-center mb-2">
             <TabPicker options={tabPickerOptions} setActiveTab={setActiveTab} />
-            {(isSRM() || isAdmin()) && <button
-              className="btn btn-primary"
-              onClick={() => setModalShow(true)}
-              style={{ marginLeft: '15px' }}
-            >
-              Add New Data
-            </button>}
+            {(isSRM() || isAdmin()) && (
+              <button
+                className="btn btn-primary"
+                onClick={() => setModalShow(true)}
+                style={{ marginLeft: "15px" }}
+              >
+                Add New Data
+              </button>
+            )}
           </div>
-          <div className={`${layout !== 'list' ? 'd-none' : ''}`}>
-           <Table onRowClick={showRowData} columns={columns} data={opts} totalRecords={optsAggregate.count} fetchData={fetchData} paginationPageSize={paginationPageSize} onPageSizeChange={setPaginationPageSize}  paginationPageIndex={paginationPageIndex} onPageIndexChange={setPaginationPageIndex} />
-
+          <div className={`${layout !== "list" ? "d-none" : ""}`}>
+            {activeTab.key == "my_data" ? (
+              <Table
+                onRowClick={showRowData}
+                columns={columns}
+                data={opts}
+                totalRecords={optsAggregate.count}
+                fetchData={fetchData}
+                paginationPageSize={paginationPageSize}
+                onPageSizeChange={setPaginationPageSize}
+                paginationPageIndex={paginationPageIndex}
+                onPageIndexChange={setPaginationPageIndex}
+              />
+            ) : activeTab.key == "useTot" ? (
+              <Table
+                onRowClick={showRowData}
+                columns={columnsUserTot}
+                data={opts}
+                totalRecords={optsAggregate.count}
+                fetchData={fetchData}
+                paginationPageSize={paginationPageSize}
+                onPageSizeChange={setPaginationPageSize}
+                paginationPageIndex={paginationPageIndex}
+                onPageIndexChange={setPaginationPageIndex}
+              />
+            ) : (
+              ""
+            )}
           </div>
         </div>
         <div className="d-flex flex-column flex-md-row justify-content-between align-items-center m-2">
-          
           <OperationCreateform
             show={modalShow}
             onHide={hideCreateModal}
-            ModalShow={()=>setModalShow(false)}
+            ModalShow={() => setModalShow(false)}
           />
           {showModal && (
-            <OperationDataupdateform 
+            <OperationDataupdateform
               {...optsdata}
               show={showModal}
               onHide={hideShowModal}
