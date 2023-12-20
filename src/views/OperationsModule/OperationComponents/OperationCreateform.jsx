4a46081697377f7c83ac7fc67e9a15c427707117
rohit-shamra-medha <rohit.sharma@medha.org.in>
2023-12-21 02:45:35 +0530

    Ops data buld add

--- src/views/OperationsModule/OperationComponents/OperationCreateform.jsx
+++ src/views/OperationsModule/OperationComponents/OperationCreateform.jsx
@@ -526,7 +526,7 @@ const OperationCreateform = (props) => {
                   <th>Institution *</th>
                   <th>State *</th>
                   <th>Medha Area *</th>
-                  <th>Program Name</th>
+                  <th>Program Name *</th>
                   <th>Batch Name *</th>
 
                   <th>Start Date *</th>
