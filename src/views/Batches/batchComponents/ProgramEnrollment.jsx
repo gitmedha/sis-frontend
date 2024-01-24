39b79da60ca672643024506c13b6baa357442889
rohit-shamra-medha <rohit.sharma@medha.org.in>
2024-01-24 18:05:14 +0530

    prollemnt is not opening and employment connection changes for apprentship

--- src/views/Batches/batchComponents/ProgramEnrollment.jsx
+++ src/views/Batches/batchComponents/ProgramEnrollment.jsx
@@ -158,7 +158,7 @@ const ProgramEnrollment = (props) => {
               <div className="col-md-6 col-sm-12">
                 <DetailField label="Program Status" value={<Badge value={programEnrollment.status} pickList={pickList.status} />} />
                 <DetailField label="Registration Date" value={programEnrollment.registration_date ? moment(programEnrollment.registration_date).format("DD MMM YYYY") : ''} />
-                <DetailField label="Program Name" value={batch?.program.name} />
+                <DetailField label="Program Name" value={batch?.program?.name} />
               </div>
               <div className="col-md-6 col-sm-12">
                 <DetailField label="Upload Assignment File" value={
