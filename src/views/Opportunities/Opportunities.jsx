8c947c8b0deb27b36f3af98f6dbf2fcd157edd59
Deepak Sharma <deepak@aayworks.com>
2024-04-19 11:54:32 +0530

    fixed the width issue for searchbar

--- src/views/Opportunities/Opportunities.jsx
+++ src/views/Opportunities/Opportunities.jsx
@@ -417,20 +417,18 @@ const opportunityQuery = `query GET_OPPORTUNITIES(
         </div>
 
         
-        <div className="d-flex justify-content-between align-items-center mb-2 px-0">
         <OpportunitySearchBar 
-        selectedSearchField={selectedSearchField} 
-        setSelectedSearchField={setSelectedSearchField} 
-        setIsSearchEnable={setIsSearchEnable}
-        setSelectedSearchedValue={setSelectedSearchedValue}
-        tab={activeTab.key}
-        info={{
-          id:userId,
-          area:area,
-          state:state,
-        }}
+          selectedSearchField={selectedSearchField} 
+          setSelectedSearchField={setSelectedSearchField} 
+          setIsSearchEnable={setIsSearchEnable}
+          setSelectedSearchedValue={setSelectedSearchedValue}
+          tab={activeTab.key}
+          info={{
+            id:userId,
+            area:area,
+            state:state,
+          }}
         />
-        </div>
         
         <Table 
           columns={columns} 
