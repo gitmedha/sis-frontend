c1446581dd1f4becd49d17fea668095970b7914a
Deepak Sharma <deepak@aayworks.com>
2024-04-24 18:45:16 +0530

    batch responsive

--- src/views/Batches/batchComponents/Details.jsx
+++ src/views/Batches/batchComponents/Details.jsx
@@ -52,45 +52,101 @@ const Details = ({ batch, sessions=[] }) => {
   const averageAttendancePercent = Math.floor(totalSessionAttendancePercentages / sessions.length);
 
   return (
-    <div className="container-fluid my-3">
-      <div className="row latto-regular">
-        <div className="col-6 col-md-4">
-          <DetailField label="Name"  value={batch.name} />
-          <DetailField label="Program Name" value={batch?.program?.name} />
-          <DetailField label="Institution" value={<Anchor text={batch?.institution?.name} href={`/institution/${batch?.institution?.id}`} />} />
-          <DetailField label="Start Date" value={<Moment date={batch.start_date} format={"DD MMM YYYY"} />} />
-          &nbsp;
-          {/* <DetailField label="Name in Current SIS" value={batch.name_in_current_sis} /> */}
-          <DetailField label="State" value={batch.state} />
-          <DetailField label="Grant" value={batch.grant?.name} />
-          &nbsp;
-          <DetailField label="Session Planned" value={batch.number_of_sessions_planned} />
-          <DetailField label="Seats Available" value={ batch.seats_available || 0} />
-          &nbsp;
-          <DetailField label="Created By" value={batch.created_by_frontend?.username ?`${batch.created_by_frontend?.username} (${batch.created_by_frontend?.email})`: ''} />
-          <DetailField label="Created At" value={moment(batch.created_at).format("DD MMM YYYY, h:mm a")} />
-          <div className="mt-2">
-            <div style={{color: '#787B96', fontFamily: 'Latto-Regular', fontSize: '14px', lineHeight: 2.2, marginBottom: '10px', marginTop: '10px'}}>
-              Average Attendance Across All Sessions
-            </div>
-            <ProgressBarField value={averageAttendancePercent} />
+    <div className="container-fluid my-3 latto-regular">
+      <div className="row">
+        <div className="row justify-content-between">
+          <div className="col-12  col-sm-5">
+            <DetailField label="Name"  value={batch.name} />
+          </div>
+          <div className="col-12  col-sm-6">
+          <DetailField label="Assigned To" value={batch.assigned_to?.username} />
           </div>
         </div>
-        <div className="col-6 offset-md-2 col-md-4">
-          <DetailField label="Assigned To" value={batch.assigned_to?.username} />
-          <DetailField label="Status" value={<Badge value={batch.status} pickList={pickList.status} />} />
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+            <DetailField label="Program Name" value={batch?.program?.name} />
+          </div>
+          <div className="col-12 col-sm-6">
+            <DetailField label="Status" value={<Badge value={batch.status} pickList={pickList.status} />} />          
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+          <DetailField label="Institution" value={<Anchor text={batch?.institution?.name} href={`/institution/${batch?.institution?.id}`} />} />
+          </div>
+          <div className="col-12 col-sm-6">
           <DetailField label="Enrollment Type" value={<Badge value={batch.enrollment_type} pickList={pickList.enrollment_type} />} />
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+          <DetailField label="Start Date" value={<Moment date={batch.start_date} format={"DD MMM YYYY"} />} />
+          &nbsp;          </div>
+          <div className="col-12 col-sm-6">
           <DetailField label="End Date" value={<Moment date={batch.end_date} format={"DD MMM YYYY"} />} />
           &nbsp;
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+          <DetailField label="State" value={batch.state} />
+          </div>
+          <div className="col-12 col-sm-6">
           <DetailField label="Area" value={batch.medha_area} />
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+          <DetailField label="Grant" value={batch.grant?.name} />
+          </div>
+          <div className="col-12 col-sm-6">
+
           <DetailField label="Donor" value={batch.grant?.donor} />
-          &nbsp;
+          &nbsp;          </div>
+        </div>
+
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+          <DetailField label="Session Planned" value={batch.number_of_sessions_planned} />
+          </div>
+          <div className="col-12 col-sm-6">
           <DetailField label="Per Student Contribution" value={batch.per_student_fees} />
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+          <DetailField label="Seats Available" value={ batch.seats_available || 0} />
+          </div>
+          <div className="col-12 col-sm-6">
           <DetailField label="Mode of Payment" value={batch.mode_of_payment} />
-          <div>&nbsp; &nbsp;</div>
-          &nbsp;
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+          <DetailField label="Created By" value={batch.created_by_frontend?.username ?`${batch.created_by_frontend?.username} (${batch.created_by_frontend?.email})`: ''} />
+          </div>
+          <div className="col-12 col-sm-6">
           <DetailField label="Updated By" value={batch.updated_by_frontend?.username ?`${batch.updated_by_frontend?.username} (${batch.updated_by_frontend?.email})`: ''} />
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-5">
+          <DetailField label="Created At" value={moment(batch.created_at).format("DD MMM YYYY, h:mm a")} />
+          </div>
+          <div className="col-12 col-sm-6">
           <DetailField label="Updated At" value={moment(batch.updated_at).format("DD MMM YYYY, h:mm a")} />
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-6">
+            <div className="mt-2">
+              <div style={{color: '#787B96', fontFamily: 'Latto-Regular', fontSize: '14px', lineHeight: 2.2, marginBottom: '10px', marginTop: '10px'}}>
+                Average Attendance Across All Sessions
+              </div>
+              <ProgressBarField value={averageAttendancePercent} />
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
