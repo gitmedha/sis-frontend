14730f0dda3670e0ce5498c966e3676fe75e361b
Deepak Sharma <deepak@aayworks.com>
2024-06-13 15:13:25 +0530

    uncommented the imp code

--- src/views/Students/StudentComponents/StudentForm.jsx
+++ src/views/Students/StudentComponents/StudentForm.jsx
@@ -111,8 +111,8 @@ const StudentForm = (props) => {
           label: item.value,
         }))
       );
-      // setHowDidYouHearAboutUsOptions(data.how_did_you_hear_about_us.map(item => ({ key: item.value, value: item.value, label: item.value })));
-      // setyourPlanFfterYourCurrentCourse(data.your_plan_after_your_current_course.map(item => ({ key: item,value: item, label: item })));
+      setHowDidYouHearAboutUsOptions(data.how_did_you_hear_about_us.map(item => ({ key: item.value, value: item.value, label: item.value })));
+      setyourPlanFfterYourCurrentCourse(data.your_plan_after_your_current_course.map(item => ({ key: item,value: item, label: item })));
     });
 
     getAddressOptions().then((data) => {
