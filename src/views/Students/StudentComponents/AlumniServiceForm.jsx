e401c1ac225ea8c12664e581a88e95d560cd5874
Rohit-Sharma-Dev <rohitshharmma@gmail.com>
2024-12-18 13:14:18 +0530

    Added Chnages for tot and alumniservice form

--- src/views/Students/StudentComponents/AlumniServiceForm.jsx
+++ src/views/Students/StudentComponents/AlumniServiceForm.jsx
@@ -423,6 +423,7 @@ const AlumniServiceForm = (props) => {
                       label="Comments"
                       placeholder="Comments"
                       control="input"
+                      required
                       className="form-control"
                       autoComplete="off"
                     ></Textarea>
