912222d2658036d78618d11977bdab435a780348
Rohit-Sharma-Dev <rohitshharmma@gmail.com>
2025-03-04 10:59:20 +0530

    Added commit for opprtunity module

--- src/views/Opportunities/OpportunityComponents/EmploymentConnections.jsx
+++ src/views/Opportunities/OpportunityComponents/EmploymentConnections.jsx
@@ -175,7 +175,7 @@ const EmploymentConnections = (props) => {
     dataToSave['salary_offered'] = data.salary_offered ? Number(data.salary_offered) : null;
     dataToSave['opportunity'] = data.opportunity_id;
     dataToSave['student'] = student_id;
-
+    dataToSave['offer_letter']=dataToSave.offer_letter ?   dataToSave.offer_letter.id  : null;
     updateEmploymentConnection(Number(id), dataToSave).then(data => {
       setAlert("Employment Connection updated successfully.", "success");
     }).catch(err => {
