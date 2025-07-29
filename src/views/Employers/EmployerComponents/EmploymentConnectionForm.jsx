2f4a97c13bb255b5fe17f2cc64ceba87019a0c51
rohit-sharma-dev <rohit.sharma@medha.org.in>
2025-07-29 14:40:29 +0530

    Added changes for employment connection

--- src/views/Employers/EmployerComponents/EmploymentConnectionForm.jsx
+++ src/views/Employers/EmployerComponents/EmploymentConnectionForm.jsx
@@ -326,7 +326,7 @@ const EnrollmentConnectionForm = (props) => {
       setRejected(true);
     } else if (value === "Student Dropped Out") {
       setRejected(true);
-    } else if (value === "Offer Rejected by Student") {
+    } else if (value === "Rejected by Student") {
       setRejected(true);
     } else {
       setRejected(false);
