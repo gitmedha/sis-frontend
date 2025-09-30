d6500857d109c1cf816243f1671eb72a1d754f4b
deepaksharma-dev <deepak.sharma@medha.org.in>
2025-09-30 12:40:01 +0530

    added changed the pitching url

--- src/views/OperationsModule/Operations.jsx
+++ src/views/OperationsModule/Operations.jsx
@@ -1618,7 +1618,7 @@ const Operations = ({
       case "upskilling":
         return "https://medhasisstg.s3.ap-south-1.amazonaws.com/Student+Upskilling+Template.xlsx";
       case "collegePitches":
-        return "https://medhasisstg.s3.ap-south-1.amazonaws.com/Pitching+Template.xlsx";
+        return "https://medhasisstg.s3.ap-south-1.amazonaws.com/Pitching%2BTemplate.xlsx";
       case "alumniQueries":
         return "https://medhasisstg.s3.ap-south-1.amazonaws.com/Alumni+Queries+Template.xlsx";
       default:
